773b295af4f87741bb17dc86cbd6628d88ef0bba
Ismael <ismaelguedessilva@gmail.com>
2023-09-28 18:12:14 -0300

    add reuse according to OOP

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
 use App\Models\Marca;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
-
+use App\Repositories\MarcaRepository;
 
 class MarcaController extends Controller
 {
@@ -18,37 +18,37 @@ class MarcaController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
-    {   $marcas = array();
+    {
 
-            //logica dos atributos
-            if($request->has('atributos_modelos')){
-                $atributos_modelo = $request->atributos_modelos;
-                $marcas = $this->marca->with('modelos:id,'.$atributos_modelo);
-            }else{
-                $marcas = $this->marca->with('modelos');
-            }
-            //logica dos filtros
-            if($request->has('filtro'))
-            {
-                $filtros = explode(';', $request->filtro);
-                    foreach($filtros as $key => $condicao){
-                        $c = explode(':',$condicao);
-                        $marcas = $marcas->where($c[0], $c[1], $c[2]);
-                    }
-                 }
+        $marcaRepository = new MarcaRepository($this->marca);
 
-
-        if($request->has('atributos')) {
-            $atributos = $request->atributos;
-            $marcas = $marcas->selectRaw($atributos)->get();
+        if($request->has('atributos_modelos')) {
+            $atributos_modelos = 'modelos:id,'.$request->atributos_modelos;
+            $marcaRepository->selectAtributosRegistrosRelacionados($atributos_modelos);
         } else {
-            $marcas = $marcas->get();
+            $marcaRepository->selectAtributosRegistrosRelacionados('modelos');
         }
 
+        if($request->has('filtro')) {
+            $marcaRepository->filtro($request->filtro);
+        }
 
-        return response()->json($marcas, 200);
+        if($request->has('atributos')) {
+            $marcaRepository->selectAtributos($request->atributos);
+        }
+
+        return response()->json($marcaRepository->getResultado(), 200);
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -58,12 +58,8 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-
         $request->validate($this->marca->rules(), $this->marca->feedback());
-        //stateless
 
-
-        //forma de upload de arquivos
         $imagem = $request->file('imagem');
         $imagem_urn = $imagem->store('imagens', 'public');
 
@@ -72,8 +68,7 @@ class MarcaController extends Controller
             'imagem' => $imagem_urn
         ]);
 
-
-        return response($imagem_urn)->json($marca, 201);
+        return response()->json($marca, 201);
     }
 
     /**
@@ -90,6 +85,17 @@ class MarcaController extends Controller
         }
 
         return response()->json($marca, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Marca $marca)
+    {
+        //
     }
 
     /**
@@ -126,24 +132,27 @@ class MarcaController extends Controller
             $request->validate($marca->rules(), $marca->feedback());
         }
 
-        //remove o arquivo de imagem antigo caso esse sofra alguma autalização
-            if($request->file('imagem')){
-                Storage::disk('public')->delete($marca->imagem);
-            }
+        //remove o arquivo antigo caso um novo arquivo tenha sido enviado no request
+        if($request->file('imagem')) {
+            Storage::disk('public')->delete($marca->imagem);
+        }
 
-            $imagem = $request->file('imagem');
-            $imagem_urn = $imagem->store('imagens', 'public');
+        $imagem = $request->file('imagem');
+        $imagem_urn = $imagem->store('imagens', 'public');
 
-            //para evitar o erro da constraint null para nome
-            $marca->fill($request->all());
-            $marca->imagem = $imagem_urn;
+        //preencher o objeto $marca com os dados do request
+        $marca->fill($request->all());
+        $marca->imagem = $imagem_urn;
+        //dd($marca->getAttributes());
+        $marca->save();
+        /*
+        $marca->update([
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn
+        ]);
+        */
 
-            $marca->save();
-
-           /* $marca->update(['nome'=> $request->nome,
-           'imagem'=> $imagem_urn]);*/
-
-           return response()->json($marca, 200);
+        return response()->json($marca, 200);
     }
 
     /**
@@ -160,10 +169,8 @@ class MarcaController extends Controller
             return response()->json(['erro' => 'Impossível realizar a exclusão. O recurso solicitado não existe'], 404);
         }
 
-            //remove o arquivo de imagem antigo caso esse sofra alguma autalização
-                Storage::disk('public')->delete($marca->imagem);
-
-
+        //remove o arquivo antigo
+        Storage::disk('public')->delete($marca->imagem);
 
         $marca->delete();
         return response()->json(['msg' => 'A marca foi removida com sucesso!'], 200);
