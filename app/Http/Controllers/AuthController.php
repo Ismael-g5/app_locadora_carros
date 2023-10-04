8c6273477537bb4ea6bf8e030ec8b09cb98ac510
Ismael <ismaelguedessilva@gmail.com>
2023-10-04 10:31:30 -0300

    fix JWT deprecated and add new configuration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,23 +6,36 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
-        return "chegamos aqui (login)";
+     //autenticação email e senha, no metodo all podemos retornar um array
+     $credenciais =  $request->all(['email', 'password']);
+
+        $token = auth('api')->attempt($credenciais);
+        if($token){
+            return response()->json(['token' => $token]);
+        }else{
+            return response()->json(['error' => 'Usuario ou Senha invalido'], 403);
+        }
+
     }
 
     public function logout()
     {
-        return "chegamos aqui (logout)";
+        auth('api')->logout(); // o proprio metodo logout joga o token para a blacklist
+        return response()->json(['message' => 'Logout realizado com sucesso'], 200);
     }
 
     public function refresh()
     {
-        return "chegamos aqui (refresh)";
+        $token = auth('api')->refresh();
+
+        return response()->json(['token' => $token]);
+            // o refresh retornara um novo token e jogara o antigo para a blacklist
     }
 
     public function me()
     {
-        return "chegamos aqui (me)";
+       return response()->json(auth()->user());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,15 +18,23 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::resource('cliente', 'App\Http\Controllers\ClienteController');
+
+Route::prefix('v1')->middleware('jwt.auth')->group(function(){
 Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
 Route::apiResource('carro', 'App\Http\Controllers\CarroController');
 Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
 Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
 Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
 
-//teste token
-Route::post('login', 'App\Http\Controllers\AuthController@login');
-Route::post('logout','App\Http\Controllers\AuthController@logout');
-Route::post('refresh','App\Http\Controllers\AuthController@refresh');
+
 Route::post('me', 'App\Http\Controllers\AuthController@me');
+Route::post('refresh','App\Http\Controllers\AuthController@refresh');
+Route::post('logout','App\Http\Controllers\AuthController@logout');
+
+
+});
+
+
+//login JWT
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+
