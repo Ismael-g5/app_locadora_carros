1d5517c72ed22f0d552ef658d00d75144238d4b8
Ismael <ismaelguedessilva@gmail.com>
2023-11-10 17:36:53 -0300

    refactoring list marcas

--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -1,16 +1,26 @@
 <template>
-       <div>
-       <table class="table table-hover">
+    <div>
+        <table class="table table-hover">
             <thead>
                 <tr>
-                    <th v-for="t, key in titulos" :key="key">{{ t }}</th>
+                    <th v-for="t, key in titulos" :key="key" class="text-uppercase">{{ t }}</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="m in dados" :key="m.id">
-                    <th scope="row">{{ m.id }}</th>
+                <tr v-for="obj in dados" :key="obj.id">
+                    <td v-if="titulos.includes(chave)" v-for="valor, chave in obj" :key="chave">
+                        <span v-if="chave == 'imagem'">
+                            <img :src="'/storage/'+valor" width="30px" height="30px">
+                        </span>
+                        <span v-else>
+                            {{ valor }}
+                        </span>
+                    </td>
+                    <!--  <th scope="row">{{ m.id }}</th>
                     <td>{{ m.nome }}</td>
                     <td><img :src="'/storage/' + m.imagem" width="30" height="30"></td>
+                  -->
+
                 </tr>
             </tbody>
         </table>
